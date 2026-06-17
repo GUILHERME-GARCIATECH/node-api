b4706ba668279ef0436abcdf0243275dff5dfc7d
Your Name <you@example.com>
2026-06-16 20:56:01 -0400

    sla

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ import express from "express";
 const server = express();
 const PORT = 3000;
 
+const customers = [
+    {
+        id: 1,
+        nome: "guilherme del toro",
+        email: "guilherme@email.com"
+    },
+    {
+        id: 2,
+        nome: "alexandre o grande",
+        email: "alexandre@email.com"
+    }
+];
+
 // http://localhost:3000/hello?nome=Guilherme&idade=18
 // Query params = ?nome=guilherme&idade=18
-
 server.get("/hello", (req, res) => {
 
     const { nome, idade } = req.query;
@@ -19,8 +31,7 @@ server.get("/hello", (req, res) => {
 
 // http://localhost:3000/hello/guilherme
 // Route params = /hello:nome
-
-server.get("/hello/:nome/:idade", (req, res) => {
+server.get("/hello/:nome", (req, res) => {
 
     const { nome, idade } = req.params;
 
@@ -31,6 +42,31 @@ server.get("/hello/:nome/:idade", (req, res) => {
     })
 });
 
+server.get("/customers", (req, res) => {
+    return res.json({
+        Title: "Lista de clientes",
+        customers: customers
+    })
+});
+
+server.get("/customers/:id", (req, res) => {
+
+    const { id } = req.params;
+
+    const customer = customers.find(c => c.id == Number(id));
+
+    if (!customer) {
+        return res.status(404).json({ error: "Cliente não encontrado" });
+    }
+
+    return res.status(200).json({
+        title: `Perfil cliente id: ${customer.nome}`,
+        customer: customer
+    })
+});
+
 server.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`)
 });
+
+
